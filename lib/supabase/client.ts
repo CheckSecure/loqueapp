cd035b279ed843a3f23cf5858ab1ac724580ed39
CheckSecure <bizdev91@gmail.com>
2026-04-04 23:19:46 -0400

    Temp: hardcode client.ts too

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    process.env.NEXT_PUBLIC_SUPABASE_URL!
+    'https://cyjyutmtsovfnnbbluxc.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5anl1dG10c292Zm5uYmJsdXhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3OTc2ODIsImV4cCI6MjA4OTM3MzY4Mn0.-_yAw3vfssLwS0CGSZLCrrFlTsfGtXHPQmUOtQoF6S4'
   )
 }
