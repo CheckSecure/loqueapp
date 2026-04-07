2447c3aa1c9b16e762e3b5e2b608425831b75bcc
CheckSecure <bizdev91@gmail.com>
2026-04-07 12:54:19 -0400

    fix: remove unsupported currentUrl prop from AvatarUpload

--- components/OnboardingStep1.tsx
+++ components/OnboardingStep1.tsx
@@ -72,7 +72,7 @@ export default function OnboardingStep1({
         <div className="h-20 bg-gradient-to-r from-[#1B2850] to-[#2E4080]" />
         <div className="px-6 pb-6">
           <div className="-mt-10 mb-4">
-            <AvatarUpload userId={profile?.id || ''} currentUrl={profile?.avatar_url} initials={initials} />
+            <AvatarUpload userId={profile?.id || ''}  initials={initials} />
           </div>
         </div>
       </div>
